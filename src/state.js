bf30410c7060e353617b48b4ae97fca0c186170f
JR <jovincent.ruiz@gmail.com>
2017-04-05 16:26:17 -0700

    updated spacing

--- src/state.js
+++ src/state.js
@@ -1,18 +1,18 @@
 const INITIAL_STATE = {
-	locations: [],
-	currentLocation: [],
-	searchQuery: '',
-	filters: [
+  locations: [],
+  currentLocation: [],
+  searchQuery: '',
+  filters: {
     isOpen: false,
     foodPantries: false,
     communityGardens: false,
     farmersMarkets: false,
     superMarkets: false
-  ],
-	pageOffset: 0,
-	isFetching: false,
-	error: '',
-	viewType: 'map'
+  },
+  pageOffset: 0,
+  isFetching: false,
+  error: '',
+  viewType: 'map'
 };
 
 export default INITIAL_STATE;
