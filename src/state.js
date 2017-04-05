a956de271d23468387da82690147310c2b98c49b
jvjruiz <jovincent.ruiz@gmail.com>
2017-04-04 20:52:12 -0700

    updated state

--- src/state.js
+++ src/state.js
@@ -1,3 +1,12 @@
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+	locations: [],
+	currentLocation: [],
+	searchQuery: '',
+	filters:[],
+	pageOffset:0,
+	isFetching:false,
+	error:'',
+	viewType: 'map'
+};
 
 export default INITIAL_STATE;
