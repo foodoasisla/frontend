7750ace1a2470fa7f636f0a76f66a89a7cc07384
JR <jovincent.ruiz@gmail.com>
2017-04-05 15:34:48 -0700

    updated formatting on state

--- src/state.js
+++ src/state.js
@@ -2,10 +2,16 @@ const INITIAL_STATE = {
 	locations: [],
 	currentLocation: [],
 	searchQuery: '',
-	filters:[],
-	pageOffset:0,
-	isFetching:false,
-	error:'',
+	filters: [
+    isOpen: false,
+    foodPantries: false,
+    communityGardens: false,
+    farmersMarkets: false,
+    superMarkets: false
+  ],
+	pageOffset: 0,
+	isFetching: false,
+	error: '',
 	viewType: 'map'
 };
 
