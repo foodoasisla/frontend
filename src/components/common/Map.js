eddeef385854e5cd77625e45a242242cc6511412
JR <jovincent.ruiz@gmail.com>
2017-04-09 16:37:04 -0700

    updated token variable

--- src/components/common/Map.js
+++ src/components/common/Map.js
@@ -4,17 +4,14 @@ import ReactMapboxGl from 'react-mapbox-gl';
 import { MAPBOX_TOKEN } from './../../utils/constants/tokens'
 
 const Map = () => {
-  const mapBoxToken = MAPBOX_TOKEN
   return (
-    <div>
-      <ReactMapboxGl
-        style="mapbox://styles/mapbox/streets-v8"
-        accessToken={mapBoxToken}
-        center={[-118.243685, 34.052234]}
-        containerStyle={{ height: '80vh', width: '80vw' }}
-      >
-      </ReactMapboxGl>
-    </div>
+    <ReactMapboxGl
+      style="mapbox://styles/mapbox/streets-v8"
+      accessToken={MAPBOX_TOKEN}
+      center={[-118.243685, 34.052234]}
+      containerStyle={{ height: '80vh', width: '80vw' }}
+    >
+    </ReactMapboxGl>
   );
 };
 
