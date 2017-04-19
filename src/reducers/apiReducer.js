9913c05c114325bebce83d68f623a15da5fbedd9
Seungjae Moon <SJM@Ashleys-MacBook-Pro-2.local>
2017-04-18 19:56:17 -0700

    delete console.log

--- src/reducers/apiReducer.js
+++ src/reducers/apiReducer.js
@@ -3,10 +3,8 @@ import INITIAL_STATE from '../state';
 const apiReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'FETCH_STARTED':
-      console.log('fetch_started');
       return true;
     case 'FETCH_FINISHED':
-      console.log('fetch_finished');
       return false;
     default:
       return state;
