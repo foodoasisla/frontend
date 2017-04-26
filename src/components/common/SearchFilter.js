55ee4ad4e81197cae5a70c41a476df0b2675406d
Seungjae Moon <SJM@Ashleys-MacBook-Pro-2.local>
2017-04-25 20:01:11 -0700

    update searchFilter component

--- src/components/common/SearchFilter.js
+++ src/components/common/SearchFilter.js
@@ -1,55 +1,63 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { updateSearchFilters } from '../../actions';
 
 
 class SearchFilter extends Component {
   render() {
+    const { 
+      isOpen, 
+      foodPantries, 
+      communityGardens, 
+      farmersMarkets,
+      superMarkets,
+      updateSearchFilters } = this.props.filters;
     return (
       <div>
       <form>
         <label>
           Is open:
           <input
-            name="isOpen"
-            type="checkbox"
-            // checked={this.props.filters.isOpen}
-            // onChange={this.handleInputChange}
+            name = "isOpen"
+            type = "checkbox"
+            checked = { isOpen }
+            onChange = { updateSearchFilters }
           />
         </label>
         <label>
           Food Pantries:
           <input
-            name="foodPantries"
-            type="checkbox"
-            // checked={this.props.filters.foodPantries}
-            // onChange={this.handleInputChange}
+            name = "foodPantries"
+            type = "checkbox"
+            checked = { foodPantries }
+            onChange = { updateSearchFilters }
           />
         </label>
         <label>
           Community Gardens:
           <input
-            name="communityGardens"
-            type="checkbox"
-            // checked={this.props.filters.communityGardens}
-            // onChange={this.handleInputChange}
+            name = "communityGardens"
+            type = "checkbox"
+            checked = { communityGardens }
+            onChange = { updateSearchFilters }
           />
         </label>
         <label>
           Farmers Markets:
           <input
-            name="farmersMarkets"
-            type="checkbox"
-            // checked={this.props.filters.farmersMarkets}
-            // onChange={this.handleInputChange}
+            name = "farmersMarkets"
+            type = "checkbox"
+            checked = { farmersMarkets }
+            onChange = { updateSearchFilters }
           />
         </label>
         <label>
           Supermarkets:
           <input
-            name="superMarkets"
-            type="checkbox"
-            // checked={this.props.filters.superMarkets}
-            // onChange={this.handleInputChange}
+            name = "superMarkets"
+            type = "checkbox"
+            checked = { superMarkets }
+            onChange = { updateSearchFilters }
           />
         </label>
         </form>
@@ -58,4 +66,19 @@ class SearchFilter extends Component {
   }
 };
 
-export default SearchFilter;
+const mapStateToProps = (state) => {
+  return { filters : state.filters }; 
+}
+
+const mapDispatchToProps = dispatch => ({
+  updateSearchFilters: (e) => {
+    const name = e.target.name;
+    const value = e.target.checked;
+    dispatch(updateSearchFilters({ prop : name, value : value }));
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(SearchFilter);
